f1fba0feb821aadefc5e5455e9aa752c3de8fda4
Justinas Globys <52034687+Globys031@users.noreply.github.com>
2023-03-10 14:35:38 +0200

    Add setup script
    This commit adds a script that sets up our application's user

---
+++ backend/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# For now I'll just have it create postgrescrutiniser, 
+# setup its home directory and create .pgpass file.
+# Later it should also setup the entirety of our application
+
+echo "Beginning setup for PostgreScrutiniser.\n"
+
+APPUSER="postgrescrutiniser"
+HOMEDIR="/usr/local/$APPUSER/"
+useradd -m -d $HOMEDIR $APPUSER
+
+echo "What is the name of the main PostgreSql database user?"
+read POSTGREUSER
+echo "What is its password?"
+read PASSWORD
+
+PORT=$(lsof -i -P -n | awk '/post/ {print $9}' | awk -F ':' '{print $2}' | head -1)
+if [ $PORT == '' ]
+then
+  echo "What port is PostgreSql running on?"
+  read PORT
+fi
+
+echo "*:$PORT:*:$POSTGREUSER:$PASSWORD" > $HOMEDIR/.pgpass
+chown $APPUSER $HOMEDIR/.pgpass
+chmod 0600 $HOMEDIR/.pgpass
