d613984c405ecde39f7e3b5f8fb959a1d486eafb
Justinas Globys <52034687+Globys031@users.noreply.github.com>
2023-04-05 14:44:44 +0300

    Improve loading spinner
    Spinner now displays "Loading..." with animated dots

--- frontend/src/components/global/UiSpinner.vue
+++ frontend/src/components/global/UiSpinner.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="loading-spinner"></div>
+  <div class="loading-container">
+    <div class="loading-spinner" />
+    <div class="loading-text">
+      Loading
+      <span
+        class="dot"
+        v-for="n in 3"
+        :key="n"
+        :style="{ animationDelay: `${n * 0.3}s` }"
+        >.</span
+      >
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
+.loading-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  margin: 20px;
+}
+
 .loading-spinner {
   border: 4px solid #f3f3f3;
   border-top: 4px solid #3498db;
@@ -12,7 +32,16 @@
   width: 20px;
   height: 20px;
   animation: spin 2s linear infinite;
-  margin: 20px auto;
+}
+
+.loading-text {
+  font-size: 14px;
+  color: #333;
+}
+
+.dot {
+  animation: fadeInOut 1.5s infinite;
+  opacity: 0;
 }
 
 @keyframes spin {
@@ -21,6 +50,16 @@
   }
   100% {
     transform: rotate(360deg);
+  }
+}
+
+@keyframes fadeInOut {
+  0%,
+  100% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
   }
 }
 </style>
